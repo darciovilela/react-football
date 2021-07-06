cd8005211154dffa143252afc3b9dcaeaebef39b
Darcio Vilela <darcio@jp7.com.br>
2021-07-06 16:19:52 -0300

    Add menu to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import { Oitenta } from './components/Oitenta';
 import { Noventa } from './components/Noventa';
 
-const App = () => {
+function App() {
+  const [page, setPage] = useState('Oitenta');
+
+  const switchPage = () => {
+    switch (page) {
+      case 'Oitenta':
+        return <Oitenta />;
+      case 'Noventa':
+        return <Noventa />;
+    }
+  };
+
   return (
     <div className="App">
-      <Header />
-      <Oitenta />
-      <Noventa />
+      <Header setpage={setPage} />
+      {switchPage()}
       <Footer />
     </div>
   );
-};
+}
 
 export default App;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,10 @@
 import logo from '../img/logo_futebol.png';
 
-export const Header = () => {
+interface IProps {
+  setpage: Function;
+}
+
+export const Header = (props: IProps) => {
   return (
     <header className="App-header">
       <div>
@@ -10,6 +14,12 @@ export const Header = () => {
         Brazil Football Champions <br />
         1980-2020
       </p>
+      <div>
+        <ul>
+          <li onClick={() => props.setpage('Oitenta')}>Anos Oitenta</li>
+          <li onClick={() => props.setpage('Noventa')}>Anos Noventa</li>
+        </ul>
+      </div>
     </header>
   );
 };
