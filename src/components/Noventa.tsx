c8232ebe584c847a6d652764c86fa3f2c6c7b890
Darcio Vilela <darcio@jp7.com.br>
2021-07-06 16:19:14 -0300

    Add connection to json server

--- src/components/Noventa.tsx
+++ src/components/Noventa.tsx
@@ -1,7 +1,31 @@
-import db from '../data/db.json';
-const noventa = db.noventa;
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export interface Noventa {
+  id?: string;
+  year: string;
+  champion: string;
+  vice: string;
+}
 
 export const Noventa = () => {
+  const [noventa, setNoventa] = useState<Noventa[]>([]);
+  const [date, setDate] = useState(+new Date());
+
+  useEffect(() => {
+    const callFetchFunction = async () => {
+      const result = await axios.get<Noventa[]>(
+        'http://localhost:4000/noventa'
+      );
+      setNoventa(result.data);
+    };
+    callFetchFunction();
+  }, [date]);
+
+  if (!noventa.length) {
+    return <div>Loading... (or empty)</div>;
+  }
+
   return (
     <div>
       <h1>1990 Champions</h1>
--- src/components/Oitenta.tsx
+++ src/components/Oitenta.tsx
@@ -1,7 +1,34 @@
-import db from '../data/db.json';
-const oitenta = db.oitenta;
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
+//declaracao do que sera passado na interface
+export interface Oitenta {
+  id?: string;
+  year: string;
+  champion: string;
+  vice: string;
+}
+
+// inicio do estado com array vazio
 export const Oitenta = () => {
+  const [oitenta, setOitenta] = useState<Oitenta[]>([]);
+  const [date, setDate] = useState(+new Date());
+
+  // componentDidMount or variable date was changed
+  useEffect(() => {
+    const callFetchFunction = async () => {
+      const result = await axios.get<Oitenta[]>(
+        'http://localhost:4000/oitenta'
+      );
+      setOitenta(result.data);
+    };
+    callFetchFunction();
+  }, [date]);
+
+  if (!oitenta.length) {
+    return <div>Loading... (or empty)</div>;
+  }
+
   return (
     <div>
       <h1>1980 Champions</h1>
